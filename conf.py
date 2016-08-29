2ea3d91d8ecd23d903c34344199009a2511f4d32
Roberto Alsina <ralsina@netmanagers.com.ar>
2016-08-29 17:13:11 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -173,11 +173,13 @@ THEME_COLOR = '#5670d4'
 #
 
 POSTS = (
+    ("posts/*.md", "posts", "post.tmpl"),
     ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.html", "posts", "post.tmpl"),
 )
 PAGES = (
+    ("stories/*.md", "stories", "story.tmpl"),
     ("stories/*.rst", "stories", "story.tmpl"),
     ("stories/*.txt", "stories", "story.tmpl"),
     ("stories/*.html", "stories", "story.tmpl"),
