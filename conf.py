c9d2ebd6c955fcf6605644803121cc834715c66c
Chris Warrick <kwpolska@gmail.com>
2020-02-17 21:11:38 +0100

    Include Identity widget to make registration work

--- conf.py
+++ conf.py
@@ -1193,7 +1193,9 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = """\
+<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+"""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
